ca138fd649a884c6260d344b79ebb45565b198b3
hysyzq <hysyzq@gmail.com>
2024-11-17 21:58:25 +1100

    working

--- back-to-kps/app.js
+++ back-to-kps/app.js
@@ -34,13 +34,13 @@ function createMarkerContent(house) {
     let scale;
     switch (house.status) {
         case '0':
-            pinColor = '#e63946'; // Red
-            borderColor = '#9d0208';
+            pinColor = '#52b788'; // Green
+            borderColor = '#081c15';
             scale = 1.0;
             break;
         case '1':
-            pinColor = '#52b788'; // Green
-            borderColor = '#081c15';
+            pinColor = '#00b4d8'; // Blue
+            borderColor = '#0077b6';
             scale = 1.0;
             break;
         case '2':
@@ -48,10 +48,15 @@ function createMarkerContent(house) {
             borderColor = '#343a40';
             scale = 0.7;
             break;
-        default:
-            pinColor = '#00b4d8'; // Default Blue
-            borderColor = '#0077b6';
+        case '3':
+            pinColor = '#e63946'; // Red
+            borderColor = '#9d0208';
             scale = 1.0;
+            break;
+        default:
+            pinColor = '#f8f9fa'; // Default
+            borderColor = '#e9ecef';
+            scale = 0.3;
     }
     const pinBackground = new google.maps.marker.PinElement({
         scale: scale,
